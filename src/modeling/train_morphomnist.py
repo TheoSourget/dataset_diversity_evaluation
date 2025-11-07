20c506dc1ddbad898d3aa9801f0cc0070269b1b6
TheoSourget <sourget.theo@live.fr>
2025-11-07 09:45:51 +0100

    Added training loop over all training scenarios

--- src/modeling/train_morphomnist.py
+++ src/modeling/train_morphomnist.py
@@ -125,115 +125,116 @@ def main(
     test_dataset = get_test_dataset()
     test_dataloader = DataLoader(test_dataset, batch_size=32)
     
-    train_dataset = lst_train_datasets.get(dataset_config)
-    if not train_dataset:
-        logger.error(f"Config name '{dataset_config}' not matching any possible configs. List of possible configs:{lst_train_datasets.keys()}")
-        return
-    
+    for ds_name in lst_train_datasets:
+        train_dataset = lst_train_datasets.get(ds_name)
+        if not train_dataset:
+            logger.error(f"Config name '{ds_name}' not matching any possible configs. List of possible configs:{lst_train_datasets.keys()}")
+            return
+        
 
-    param_message = f"TRANING INFO\nDATASET CONFIG: {dataset_config}\n"
-    param_message += f"NUMBER OF EPOCHS: {epochs}\n"
-    param_message += f"Learning rate: {lr}\n"
-    param_message += f"BATCH SIZE: {batch_size}"
+        param_message = f"TRANING INFO\nDATASET CONFIG: {ds_name}\n"
+        param_message += f"NUMBER OF EPOCHS: {epochs}\n"
+        param_message += f"Learning rate: {lr}\n"
+        param_message += f"BATCH SIZE: {batch_size}"
 
-    training_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-    training_folder = INTERIM_DATA_DIR / f"morphomnist_trainings/{dataset_config}_{training_datetime}"
-    Path(training_folder).mkdir(parents=True, exist_ok=True)
-    with open(training_folder/"parameters","w") as metrics_csvfile:
-        metrics_csvfile.write(param_message)
-    logger.info(param_message)
+        training_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+        training_folder = INTERIM_DATA_DIR / f"morphomnist_trainings/{ds_name}_{training_datetime}"
+        Path(training_folder).mkdir(parents=True, exist_ok=True)
+        with open(training_folder/"parameters","w") as metrics_csvfile:
+            metrics_csvfile.write(param_message)
+        logger.info(param_message)
 
 
-    skf = StratifiedKFold(n_splits=5)
-    if isinstance(train_dataset,ConcatDataset):
-        X = train_dataset.datasets[0].labels_csv
-        y = train_dataset.datasets[0].labels_csv["label"]
-    else:
-        X = train_dataset.labels_csv
-        y = train_dataset.labels_csv["label"]
-    
-    for i, (train_index, val_index) in enumerate(skf.split(X, y)):
-        logger.info(f"Starting epoch {i}")
-        #Create the training and validation splits
-        #In case of combination of datasets (ConcatDataset), we compute the train and val split indexes on the first dataset and take the same indexes
-        #for the other datasets to keep the same "base" digit and avoid data leakage
+        skf = StratifiedKFold(n_splits=5)
         if isinstance(train_dataset,ConcatDataset):
-            lst_datasets_train = []
-            lst_datasets_val = []
-            for _,d in enumerate(train_dataset.datasets):
-                #Copy the dataset, filter to keep training samples and add to the list used later to create the ConcatDataset
-                d_copy_train = deepcopy(d)
-                d_copy_train.labels_csv = d_copy_train.labels_csv.iloc[train_index].reset_index(drop=True)
-                d_copy_train.imgs = d_copy_train.imgs[train_index]
-                lst_datasets_train.append(d_copy_train)
-                
-                #Similar to training samples but for the validation split
-                d_copy_val = deepcopy(d)
-                d_copy_val.labels_csv = d_copy_val.labels_csv.iloc[val_index].reset_index(drop=True)
-                d_copy_val.imgs = d_copy_val.imgs[val_index]
-                lst_datasets_val.append(d_copy_val)
-            
-            #Create the ConcatDataset from the list of filtered datasets
-            train_fold = ConcatDataset(lst_datasets_train)
-            train_fold.as_tensor = train_dataset.datasets[0].as_tensor
-            
-            val_fold = ConcatDataset(lst_datasets_val)
-            val_fold.as_tensor = train_dataset.datasets[0].as_tensor
+            X = train_dataset.datasets[0].labels_csv
+            y = train_dataset.datasets[0].labels_csv["label"]
         else:
-            #Case of single dataset, filter the data to keep the training samples only
-            train_fold = deepcopy(train_dataset)
-            train_fold.labels_csv = train_fold.labels_csv.iloc[train_index].reset_index(drop=True)
-            train_fold.imgs = train_fold.imgs[train_index]
-            
-            #Same for validation split
-            val_fold = deepcopy(train_dataset)
-            val_fold.labels_csv = val_fold.labels_csv.iloc[val_index].reset_index(drop=True)
-            val_fold.imgs = val_fold.imgs[val_index]
+            X = train_dataset.labels_csv
+            y = train_dataset.labels_csv["label"]
         
-        #Create the Dataloaders
-        train_dataloader = DataLoader(train_fold, batch_size=batch_size)
-        valid_dataloader = DataLoader(val_fold, batch_size=batch_size)
-        
-        #Instanciate the model, criterion and optimizer
-        model = resnet50()
-        model.fc = torch.nn.Linear(model.fc.in_features, 10)
-
-        criterion = torch.nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(),lr=lr)
-
-        datamaps_info = {}
-        #Training/Evaluation loop
-        for e in tqdm(range(epochs)):
-            train_loss,aucs_val = training_epoch(model,criterion,optimizer,train_dataloader)
-            val_loss,aucs_val = valid_epoch(model,criterion,valid_dataloader)
-
-            #Compute pred on test set for datamap
-            lst_img_ids,lst_dataset_names,lst_labels,lst_probas = compute_datamap_info(model,test_dataloader)
-            for j in range(len(lst_img_ids)):
-                sample_id = f"{lst_img_ids[j]}_{lst_dataset_names[j]}"
-                if sample_id not in datamaps_info:
-                    datamaps_info[sample_id] = {
-                        "img_id":lst_img_ids[j],
-                        "dataset_name":lst_dataset_names[j],
-                        "label":lst_labels[j],
-                        "proba_label":[float(lst_probas[i][int(lst_labels[j])])]
-                    }
-                else:
-                    datamaps_info[sample_id]["proba_label"].append(float(lst_probas[j][int(lst_labels[j])]))
+        for i, (train_index, val_index) in enumerate(skf.split(X, y)):
+            logger.info(f"Starting fold {i}")
+            #Create the training and validation splits
+            #In case of combination of datasets (ConcatDataset), we compute the train and val split indexes on the first dataset and take the same indexes
+            #for the other datasets to keep the same "base" digit and avoid data leakage
+            if isinstance(train_dataset,ConcatDataset):
+                lst_datasets_train = []
+                lst_datasets_val = []
+                for _,d in enumerate(train_dataset.datasets):
+                    #Copy the dataset, filter to keep training samples and add to the list used later to create the ConcatDataset
+                    d_copy_train = deepcopy(d)
+                    d_copy_train.labels_csv = d_copy_train.labels_csv.iloc[train_index].reset_index(drop=True)
+                    d_copy_train.imgs = d_copy_train.imgs[train_index]
+                    lst_datasets_train.append(d_copy_train)
+                    
+                    #Similar to training samples but for the validation split
+                    d_copy_val = deepcopy(d)
+                    d_copy_val.labels_csv = d_copy_val.labels_csv.iloc[val_index].reset_index(drop=True)
+                    d_copy_val.imgs = d_copy_val.imgs[val_index]
+                    lst_datasets_val.append(d_copy_val)
+                
+                #Create the ConcatDataset from the list of filtered datasets
+                train_fold = ConcatDataset(lst_datasets_train)
+                train_fold.as_tensor = train_dataset.datasets[0].as_tensor
+                
+                val_fold = ConcatDataset(lst_datasets_val)
+                val_fold.as_tensor = train_dataset.datasets[0].as_tensor
+            else:
+                #Case of single dataset, filter the data to keep the training samples only
+                train_fold = deepcopy(train_dataset)
+                train_fold.labels_csv = train_fold.labels_csv.iloc[train_index].reset_index(drop=True)
+                train_fold.imgs = train_fold.imgs[train_index]
+                
+                #Same for validation split
+                val_fold = deepcopy(train_dataset)
+                val_fold.labels_csv = val_fold.labels_csv.iloc[val_index].reset_index(drop=True)
+                val_fold.imgs = val_fold.imgs[val_index]
             
-            pd.DataFrame.from_dict(datamaps_info, orient='index').to_csv(training_folder/f"datamaps_values_fold{i}.csv")
+            #Create the Dataloaders
+            train_dataloader = DataLoader(train_fold, batch_size=batch_size)
+            valid_dataloader = DataLoader(val_fold, batch_size=batch_size)
+            
+            #Instanciate the model, criterion and optimizer
+            model = resnet50()
+            model.fc = torch.nn.Linear(model.fc.in_features, 10)
 
-            torch.save({
-                'optimizer': optimizer.state_dict(),
-                'model': model.state_dict(),
-                'last_epoch':e
-            }, training_folder/f"checkpoint_fold{i}.pth")
+            criterion = torch.nn.CrossEntropyLoss()
+            optimizer = optim.Adam(model.parameters(),lr=lr)
 
-            with open(training_folder/f"training_loss_fold{i}.csv","a") as metrics_csvfile:
-                metrics_csvfile.write(f"{train_loss},{val_loss}\n")
-            print(train_loss,val_loss)
+            datamaps_info = {}
+            #Training/Evaluation loop
+            for e in tqdm(range(epochs)):
+                train_loss,aucs_val = training_epoch(model,criterion,optimizer,train_dataloader)
+                val_loss,aucs_val = valid_epoch(model,criterion,valid_dataloader)
 
-        logger.success(f"Completed epoch {e}")
+                #Compute pred on test set for datamap
+                lst_img_ids,lst_dataset_names,lst_labels,lst_probas = compute_datamap_info(model,test_dataloader)
+                for j in range(len(lst_img_ids)):
+                    sample_id = f"{lst_img_ids[j]}_{lst_dataset_names[j]}"
+                    if sample_id not in datamaps_info:
+                        datamaps_info[sample_id] = {
+                            "img_id":lst_img_ids[j],
+                            "dataset_name":lst_dataset_names[j],
+                            "label":lst_labels[j],
+                            "proba_label":[float(lst_probas[j][int(lst_labels[j])])]
+                        }
+                    else:
+                        datamaps_info[sample_id]["proba_label"].append(float(lst_probas[j][int(lst_labels[j])]))
+                
+                pd.DataFrame.from_dict(datamaps_info, orient='index').to_csv(training_folder/f"datamaps_values_fold{i}.csv")
+
+                torch.save({
+                    'optimizer': optimizer.state_dict(),
+                    'model': model.state_dict(),
+                    'last_epoch':e
+                }, training_folder/f"checkpoint_fold{i}.pth")
+
+                with open(training_folder/f"training_loss_fold{i}.csv","a") as metrics_csvfile:
+                    metrics_csvfile.write(f"{train_loss},{val_loss}\n")
+                print(train_loss,val_loss)
+
+            logger.success(f"Completed fold {i}")
     
 if __name__ == "__main__":
     app()
