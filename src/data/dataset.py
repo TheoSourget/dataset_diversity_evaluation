8fee53c04c363407a6747dd15ecee8b62445a294
TheoSourget <sourget.theo@live.fr>
2026-01-20 09:09:14 +0100

    Processing CXR14 with NEATX annotations

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -16,6 +16,7 @@ import ast
 import tensorflow as tf
 import glob
 from sklearn.model_selection import GroupShuffleSplit,StratifiedGroupKFold
+import cv2
 
 app = typer.Typer()
 
@@ -117,10 +118,6 @@ def train_test_split_padchest():
     train_labels = labels.iloc[train_idx].reset_index(drop=True)
     test_labels = labels.iloc[test_idx].reset_index(drop=True)
     
-    # print(labels["label"].value_counts(),train_labels["label"].value_counts(),test_labels["label"].value_counts())
-    # print(labels[labels["label"]==1]["PatientSex_DICOM"].value_counts(),train_labels[train_labels["label"]==1]["PatientSex_DICOM"].value_counts(),test_labels[test_labels["label"]==1]["PatientSex_DICOM"].value_counts())
-    # print(labels[labels["label"]==1]["Manufacturer_DICOM"].value_counts(),train_labels[train_labels["label"]==1]["Manufacturer_DICOM"].value_counts(),test_labels[test_labels["label"]==1]["Manufacturer_DICOM"].value_counts())
-
     train_labels.to_csv(f"{PROCESSED_DATA_DIR}/padchest/train_labels.csv",sep=",")
     test_labels.to_csv(f"{PROCESSED_DATA_DIR}/padchest/test_labels.csv",sep=",")
 
@@ -133,6 +130,22 @@ def process_padchest():
     padchest_process_images()
     logger.info("Train-Test split of PadChest...")
     train_test_split_padchest()
+
+
+def process_CXR14():
+    #Load CXR14 and NEATX annotations
+    cxr14_annotations = pd.read_csv(f"{RAW_DATA_DIR}/CXR14/Data_Entry_2017.csv")
+    neatx_annotations = pd.read_csv(f"{RAW_DATA_DIR}/CXR14/NIH-CX14_TubeAnnotations_NonExperts_aggregated.csv")
+
+    processed_annotation = pd.merge(cxr14_annotations,neatx_annotations,on=["Image Index"],how="left")
+    processed_annotation["Drain"] = processed_annotation["Drain"].fillna(-1)
+    processed_annotation["Pneumothorax"] = processed_annotation["Finding Labels"].apply(lambda x:"Pneumothorax" in x)
+    processed_annotation.to_csv(f"{PROCESSED_DATA_DIR}/CXR14/processed_labels.csv")
+    for img_id in neatx_annotations["Image Index"]:
+        img = cv2.imread(f"{RAW_DATA_DIR}/CXR14/imgs/{img_id}")
+        resized_img = cv2.resize(img, (512, 512))
+        normalized_image = cv2.normalize(resized_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+        cv2.imwrite(f"{PROCESSED_DATA_DIR}/CXR14/imgs/{img_id}", normalized_image)
 
 @app.command()
 def main():
@@ -147,6 +160,11 @@ def main():
     logger.info("Processing PadChest dataset...")
     process_padchest()
     logger.success("Processing PadChest complete.")
+
+    logger.info("Processing CXR14 dataset...")
+    process_CXR14()
+    logger.success("Processing CXR14 complete.")
+
 
 if __name__ == "__main__":
     app()
